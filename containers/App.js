4cff082720a2276ba23f2d179a71ee4c21d6397a
Darrell Cooper <dcooper163@gmail.com>
2018-12-30 13:48:30 -0500

    Created React application for Robo Friends App

---
+++ containers/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import CardList from '../components/CardList';
+//import SearchBox from '../components/SearchBox';
+//import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+
+render(){
+return(
+
+<div>
+    React App
+</div>
+
+);
+
+
+}
+
+
+}
+
+export default App;
---
+++ robots.js
@@ -0,0 +1,16 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Darrell Cooper',
+    username: 'Darrell',
+    email: 'darrell@clickmotion.com'
+},
+{
+    id: 2,
+    name: 'Joe Stein',
+    username: 'Joe',
+    email: 'joe@gmail.com'
+}
+
+
+];
