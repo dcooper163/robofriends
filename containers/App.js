2ad19883d4b795a0b153bed2fc4d10624c241dd5
Darrell Cooper <dcooper163@gmail.com>
2018-12-30 22:08:40 -0500

    Fetch json data from API. Randomize and loop through images by changing parameter and caling web service at https://robohash.org

--- containers/App.js
+++ containers/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-//import CardList from '../components/CardList';
+import CardList from '../components/CardList';
 //import SearchBox from '../components/SearchBox';
 //import Scroll from '../components/Scroll';
 import './App.css';
 
+
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: []
+  //          searchfield: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => {this.setState( { robots: users })});
+    }
+
+onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+}
+
 
 render(){
-return(
+    const { robots } = this.state;
+//    const filterdRobots = robots.filter(robot =>{
+//        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//    })
 
-<div>
+    return !robots.length ?
+    <h1> Loading</h1> :
+    (
+
+<div className='tc'>
     React App
+    <CardList robots={robots} />
 </div>
-
 );
 
 
